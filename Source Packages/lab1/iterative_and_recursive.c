13037bb3af0740499d9c11c2a0ca7cb1def4849f
Antonio <antonio_97@live.se>
2018-09-10 13:39:19 +0200

    small changes

--- Source Packages/lab1/iterative_and_recursive.c
+++ Source Packages/lab1/iterative_and_recursive.c
@@ -1,13 +1,13 @@
 /*
 *   "README" - comment.
 *   The functions "iterative_reverse" and "recursive_reverse" both accomplish
-*   the same task, to read the standard input stream until newline and put it
+*   the same task; to read the standard input stream until newline and put it
 *   in the standard output stream reversed. The difference is the implementation
 *   of the functions.
 *
 *   The "iterative_reverse" function iterates through the input stream until newline
-*   saving each char in an array (up to 100), then it outputs each char of the array
-*   starting from the last char to the output stream.
+*   saving each char in an array and if necessary reallocates more memory, then it
+*   outputs each char of the array starting from the last char to the output stream.
 *
 *   The "recursive_reverse" function reads the first char of the input stream and
 *   if it is not equal to '\n' it will recursivly call itself until the char is equal
@@ -16,31 +16,39 @@
 */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+#define INIT_ARRAY_SIZE 100
+
+/*
+* Allocates initial memory and reads each char from stdin until newline is read.
+* Then prints it out in the reversed order by iterating from the last char to the first.
+*/
 void iterative_reverse(void)
 {
-    int size = 10;
     int i = 0;
-    int j;
     char c;
-    char *str = malloc(10*sizeof(char));
+    char *str = malloc(INIT_ARRAY_SIZE*sizeof(char));
 
     while((c = getchar()) != '\n')
     {
         str[i++] = c;
-        if(i >= size)
-        {
-            str = (char *) realloc(str, size*2);
-            size = (size*2);
-        }
+        if(i >= strlen(str))
+            str = (char *) realloc(str, strlen(str)*2);
     }
 
-    for (j = i - 1; j >= 0; j--)
-        putchar(str[j]);
+    for (i = i - 1; i >= 0; i--)
+        putchar(str[i]);
 
     free (str);
 }
 
+/*
+* Reads each char and if it is not a newline it calls itself recursivly. This will
+* save each char and the caller in a stack and when newline is reached it will return
+* to the caller function and print out the char that was read. In this way the chars
+* will be printed out in a reversed order.
+*/
 void recursive_reverse(void)
 {
     char c;
